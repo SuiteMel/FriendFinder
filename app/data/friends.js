709a244165c40f3e49e59ef361d66631570f9011
Melissa Waltner <melissamwaltner@gmail.com>
2018-04-21 02:47:08 -0500

    Created files

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendsArray = [
+  {
+    name: "Cierra",
+    photo: "https://randomuser.me/api/portraits/lego/8.jpg",
+    scores:[4, 2, 2, 5, 4, 5, 5, 4, 2, 3]
+  },
+  {
+    name: "Connor",
+    photo: "https://randomuser.me/api/portraits/lego/3.jpg",
+    scores:[5, 5, 1, 5, 3, 4, 4, 2, 5, 1]
+  }
+];
+
+module.exports = friendsArray;
